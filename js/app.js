5efd9efe93603a626ab9759416709586d1e55a7c
mrmurtz <murtaza.s.abidi@gmail.com>
2016-10-13 19:03:33 +0100

    newData is not being passed into .flowchart final method

--- js/app.js
+++ js/app.js
@@ -1,19 +1,3 @@
-function Operator(top, left, title){
-  this.framework = {
-          top: 0,
-          left: 0,
-          properties: {
-            title: '',
-            inputs: {},
-            outputs: {
-            }
-          }
-        };
-      }
-
-var UserSelection = {language: "1", framework: "1", orm: "1", db: "1", tableName1: "photo", tableName2: "user", tableName3: "tag", columnName1: "name", columnName2: "image", columnName3: "id"};
-
-// for each table create a new Operator framework and title it with tableName1
 var UseThis = {
                 	"language": "1",
                 	"framework": "1",
@@ -32,41 +16,102 @@ var UseThis = {
                 					"type": "string"
                 				}
                 			}
-                		}
+                		},
+                    "2": {
+                      "tableName": "tag",
+                      "columns": {
+                        "1": {
+                          "name": "first col name",
+                          "type": "string"
+                        },
+                        "2": {
+                          "name": "second col name",
+                          "type": "string"
+                        }
+                      }
+                    }
                 	}
                 };
-var count = Object.keys(UseThis.table).length;
-var colCount = Object.keys(UseThis.table["1"].columns).length;
 
-function OperatorCreator(UserSelection){
-  var tableLength = 3;
-  for (var i = 1; i < tableLength+1; i++) {
-    var currentTable = UserSelection.table.i;
-    var top = currentTable*20;
-    var left = currentTable*20;
-    var title = currentTable.tableName;
-    console.log(top);
-    console.log(left);
-    console.log(title);
-    operator+i = new Operator(top, left, title);
-  }
-}
-
-function updateOperator(operator){
-  var noOfColumns = 3;
-  var columnData = operator.framework.properties.outputs;
-  var result = {};
-  for (var i = 1; i < noOfColumns; i++) {
-
+  // COLUMN FUNCTIONS
+  function columnCreator(json){
+    var noOfColumns = 3;
+    var outputs = [];
+    for (var i = 1; i < noOfColumns; i++) {
+      var columnData = json.table[i].columns[i];
+      outputs.push(columnData);
+    }
+    return outputs;
   }
 
+
+  function OperatorCreator(json){
+    var tableLength = 2;
+    var all = [];
+    var columnsArray = columnCreator(json);
+    var column1 = columnsArray[0];
+    var column2 = columnsArray[1];
+    for (var i = 1; i < tableLength+1; i++) {
+      var currentTable = json.table[i];
+      var top = 20;
+      var left = i*200;
+      var title = currentTable.tableName;
+      var output = new Operator(top, left, title);
+      output.operator.properties.outputs = columnsArray[i - 1];
+      all.push(output);
+    }
+    return all;
+  }
+
+// DATA SHELL
+
+function Operator(top, left, title){
+  this.operator = {
+          top: top,
+          left: left,
+          properties: {
+            title: title,
+            inputs: {},
+            outputs: {
+            }
+          }
+        };
+      }
+
+// TABLE FUNCTIONS
+
+
+function convertToData(array) {
+  var data = {
+    operators: {
+    },
+    links: {
+    }
+  };
+  data.operators["operator1"] = array[0].operator;
+  data.operators["operator2"] = array[1].operator;
+  return data;
 }
 
 
+$(document).ready(function() {
+  var array = OperatorCreator(UseThis);
+  var newData = convertToData(array);
+  console.log(newData);
+    $('#row').flowchart({
+      data: newData
+    });
+  });
 
 
-//
-//
+
+// BELOW ARE DIFFERENT WAYS OF MAKING THE AMOUNT OF MODELS CUSTOMIZABLE
+
+  // var count = Object.keys(UseThis.table).length;
+  // var colCount = Object.keys(UseThis.table["1"].columns).length;
+
+
+
 // var tableNames = [];
 // tableNames.push(UserSelection.tableName1);
 //  if (UserSelection.tableName2 !== null) {
@@ -82,47 +127,3 @@ function updateOperator(operator){
 // var arrayLength = tableNames.filter( String );
 // var length = arrayLength.length;
 //
-//
-
-
-
-// get all the properties and add them in a new framework
-//  add this framework to data.operators....
-
-
-
-var data = {
-  operators: {
-
-  },
-  links: {
-  }
-};
-
-
-
-//
-// for (var i = 0; i < 4; i++) {
-//   // grab table lenght
-//   // loop through table
-//   // operator new with
-//   test.operators["operator"+i] = test;
-//   test.this.framework.properties.title = "user";
-// }
-//
-// console.log(test);
-
-
-
-
-
-
-
-
-
-
-$(document).ready(function(){
-  $('#row').flowchart({
-  data: data
-});
-});
